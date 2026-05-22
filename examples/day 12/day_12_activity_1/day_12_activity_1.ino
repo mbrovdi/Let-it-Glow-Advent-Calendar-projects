d57d9c7855ead95a7792d9d841203151d4e7bd73
Mariana Brovdi <mariana@morganelectronics.co.uk>
2026-05-22 13:58:29 +0100

    Set up LCD display with greeting message
    Initialize LCD and display 'Hello, World!'

--- examples/day 12/day_12_activity_1/day_12_activity_1.ino
+++ examples/day 12/day_12_activity_1/day_12_activity_1.ino
@@ -14,7 +14,21 @@ SCL connected to A5
 
 Code: 
 
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();          // Initialize the LCD
+  lcd.backlight();     // Turn on the backlight
+
+  lcd.setCursor(0, 0); // Column 0, Row 0
+  lcd.print("Hello, World!");
+}
+
+void loop() {
+  // Nothing needed here
+}
