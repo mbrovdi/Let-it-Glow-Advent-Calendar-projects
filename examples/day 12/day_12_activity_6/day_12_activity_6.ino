e5f30922ac972b3e607e6daaa7fdf727aac2c3ca
Mariana Brovdi <mariana@morganelectronics.co.uk>
2026-05-22 14:04:47 +0100

    Implement text scrolling on LCD display
    Added a function to scroll text left on the LCD.

--- examples/day 12/day_12_activity_6/day_12_activity_6.ino
+++ examples/day 12/day_12_activity_6/day_12_activity_6.ino
@@ -18,3 +18,33 @@ void setup() {
 
 
 void loop() {
+  scrollLeft(scrollText);
+}
+
+
+void scrollLeft(String text) {
+  // Add 16 spaces to the end of the text
+  text += "                ";
+
+
+  while (true) {
+    // Always start at position 0,0
+    lcd.setCursor(0, 0);
+
+
+    // Show the first 16 characters of the string
+    lcd.print(text.substring(0, 16));
+
+
+    delay(scrollDelay);
+
+
+    // Update 'text' for the next loop:
+    // Remove the first character and add it to the end
+    text = text.substring(1) + text[0];
+
+
+    // (Optional) Print to Serial for debugging
+    // Serial.println(text);
+  }
+}
