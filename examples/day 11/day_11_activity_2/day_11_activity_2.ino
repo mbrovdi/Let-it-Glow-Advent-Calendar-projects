8e97244333efe7786e4e93fe97250276e07f04d3
Mariana <mariana@morganelectronics.co.uk>
2026-05-25 16:41:31 +0100

    Fix Day 11 linker errors in activity 2 and 3
    Restored missing setup/loop logic in Day 11 activity 2 and activity 3 sketches after partial undo/truncation

--- examples/day 11/day_11_activity_2/day_11_activity_2.ino
+++ examples/day 11/day_11_activity_2/day_11_activity_2.ino
@@ -15,3 +15,43 @@ Adafruit_NeoPixel strand(LED_COUNT, STRAND_PIN, NEO_RGB + NEO_KHZ800);
 
 uint32_t aqua  = strand.Color(0, 255, 255);
 uint32_t green = strand.Color(0, 255, 0);
+uint32_t white = strand.Color(255, 255, 255);
+uint32_t blue  = strand.Color(0, 0, 255);
+uint32_t yellow = strand.Color(255, 255, 0);
+
+void fillOddEven(uint32_t evenColor, uint32_t oddColor) {
+	for (int i = 0; i < LED_COUNT; i++) {
+		if ((i % 2) == 0) {
+			strand.setPixelColor(i, evenColor);
+		} else {
+			strand.setPixelColor(i, oddColor);
+		}
+	}
+	strand.show();
+}
+
+void setup() {
+	pinMode(KEY1_PIN, INPUT_PULLDOWN);
+	pinMode(KEY2_PIN, INPUT_PULLDOWN);
+	pinMode(KEY3_PIN, INPUT_PULLDOWN);
+	pinMode(KEY4_PIN, INPUT_PULLDOWN);
+
+	strand.begin();
+	strand.clear();
+	strand.show();
+}
+
+void loop() {
+	if (digitalRead(KEY1_PIN) == HIGH) {
+		fillOddEven(green, aqua);
+	} else if (digitalRead(KEY2_PIN) == HIGH) {
+		fillOddEven(white, blue);
+	} else if (digitalRead(KEY3_PIN) == HIGH) {
+		fillOddEven(yellow, green);
+	} else if (digitalRead(KEY4_PIN) == HIGH) {
+		strand.clear();
+		strand.show();
+	}
+
+	delay(120);
+}
--- examples/day 11/day_11_activity_3/day_11_activity_3.ino
+++ examples/day 11/day_11_activity_3/day_11_activity_3.ino
@@ -13,3 +13,25 @@ void setup() {
   pinMode(KEY2_PIN, INPUT_PULLDOWN);
   pinMode(KEY3_PIN, INPUT_PULLDOWN);
   pinMode(KEY4_PIN, INPUT_PULLDOWN);
+  pinMode(BLOCK_LED_PIN, OUTPUT);
+  digitalWrite(BLOCK_LED_PIN, LOW);
+}
+
+void loop() {
+  int flashDelay = 500;
+
+  if (digitalRead(KEY1_PIN) == HIGH) {
+    flashDelay = 120;
+  } else if (digitalRead(KEY2_PIN) == HIGH) {
+    flashDelay = 250;
+  } else if (digitalRead(KEY3_PIN) == HIGH) {
+    flashDelay = 500;
+  } else if (digitalRead(KEY4_PIN) == HIGH) {
+    flashDelay = 1000;
+  }
+
+  digitalWrite(BLOCK_LED_PIN, HIGH);
+  delay(flashDelay);
+  digitalWrite(BLOCK_LED_PIN, LOW);
+  delay(flashDelay);
+}
